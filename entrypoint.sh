3e69a237a0a8a5599b83a1404f53a7354b5a9e82
Thomas GUIRRIEC <thomas@guirriec.fr>
2020-05-11 11:03:12 +0200

    Update Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@ set -eu
 
 CVD_FILE="/var/lib/clamav/main.cvd"
 
+# Check For Existing ClamAV Database
 if [ ! -f ${CVD_FILE} ]; then
     echo "[Entrypoint] Download ClamAV Database"
     /usr/bin/freshclam
 fi
 
+# Update ClamAV Database
 echo "[Entrypoint] Update ClamAV Database"
 /usr/bin/freshclam -d -c 12
 
+# Run ClamAV Daemon
 echo "[Entrypoint] Run ClamAV Daemon"
 exec /usr/sbin/clamd
