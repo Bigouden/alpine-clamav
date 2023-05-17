4306df444f75ad1edfce6faa0476ce32bb95d36a
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-05-17 15:58:02 +0000

    Mega Linter Integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ CVD_FILE="/var/lib/clamav/main.cvd"
 
 # Check For Existing ClamAV Database
 if [ ! -f ${CVD_FILE} ]; then
-    echo "[ClamAV/Freshclam] Download ClamAV Database"
-    /usr/bin/freshclam
+	echo "[ClamAV/Freshclam] Download ClamAV Database"
+	/usr/bin/freshclam
 fi
 
 # Update ClamAV Database
@@ -17,10 +17,9 @@ echo "[ClamAV/Freshclam] Update ClamAV Database"
 ################
 
 # Run ClamAV Daemon
-if [ -z "${CI}" ]
-then
-    echo "[ClamAV/Freshclam] Run ClamAV Daemon"
-    exec /usr/sbin/clamd
+if [ -z "${CI}" ]; then
+	echo "[ClamAV/Freshclam] Run ClamAV Daemon"
+	exec /usr/sbin/clamd
 fi
 
 ###############
@@ -28,22 +27,19 @@ fi
 ###############
 
 # Standalone Mode
-if [ "${1}" = "sh" ]
-then
-    # Start CLAMAV (Background)
-    /usr/sbin/clamd &
-    # Wait For Working CLAMAV
-    while [ ! "$(echo PING | nc localhost 3310)" = "PONG" ]
-    do
-        sleep 0.5
-    done
-    # Launch Shell
-    /bin/sh
-elif [ "${1}" = "service" ]
-then
-    # Start CLAMAV (Foreground)
-    /usr/sbin/clamd
+if [ "${1}" = "sh" ]; then
+	# Start CLAMAV (Background)
+	/usr/sbin/clamd &
+	# Wait For Working CLAMAV
+	while [ ! "$(echo PING | nc localhost 3310)" = "PONG" ]; do
+		sleep 0.5
+	done
+	# Launch Shell
+	/bin/sh
+elif [ "${1}" = "service" ]; then
+	# Start CLAMAV (Foreground)
+	/usr/sbin/clamd
 else
-    echo "Invalid ${1} Argument !"
-    exit 1
+	echo "Invalid ${1} Argument !"
+	exit 1
 fi
--- healthcheck.sh
+++ healthcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ "$(echo PING | nc localhost 3310)" = "PONG" ]; then
-    echo "ClamAV Daemon OK"
+	echo "ClamAV Daemon OK"
 else
-    echo "ClamAV Daemon KO"
-    exit 1
+	echo "ClamAV Daemon KO"
+	exit 1
 fi
